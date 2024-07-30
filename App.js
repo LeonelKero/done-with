e38cb8713ab9470ef565cac154fa319b580cc4f3
leonel ka <waboleonel@gmail.com>
2024-07-30 21:55:36 +0100

    Debugging - ImageInput

--- App.js
+++ App.js
@@ -3,17 +3,19 @@ import { StyleSheet } from "react-native";
 import DWImageInputList from "./app/components/input/DWImageInputList";
 import color from "./app/config/color";
 import RootContainer from "./app/screens/RootContainer";
+import DWText from "./app/components/text/DWText";
 
 const sample = require("./app/assets/necher.jpeg");
 
 export default function App() {
-  const [imageUris, setImageUris] = useState<ImageURISource>([]);
+  const [imageUris, setImageUris] = useState([]);
 
   const handleAdd = (uri) => {};
 
   return (
     <RootContainer>
       <DWImageInputList imageUris={imageUris} onAddImage={handleAdd} />
+      <DWText>Some text</DWText>
     </RootContainer>
   );
 }
