c6ef7bff74822df6734a3b9eaa68eb1b8664a966
leonel ka <waboleonel@gmail.com>
2024-07-31 06:14:31 +0100

    Accessing Camera

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 import DWImageInputList from "./app/components/input/DWImageInputList";
 import color from "./app/config/color";
 import RootContainer from "./app/screens/RootContainer";
@@ -8,19 +9,30 @@ import DWText from "./app/components/text/DWText";
 const sample = require("./app/assets/necher.jpeg");
 
 export default function App() {
-  const [imageUris, setImageUris] = useState([]);
+  // const [imageUris, setImageUris] = useState([]);
 
-  const handleAdd = (uri) => setImageUris([...imageUris, uri]);
-  const handleRemove = (uri) =>
-    setImageUris(imageUris.filter((img) => img !== uri));
+  // const handleAdd = (uri) => setImageUris([...imageUris, uri]);
+  // const handleRemove = (uri) =>
+  //   setImageUris(imageUris.filter((img) => img !== uri));
+
+  const requestCameraPermission = async () => {
+    const permission = await ImagePicker.requestCameraPermissionsAsync();
+    if (!permission.granted)
+      alert("This application needs acces to Camera in order to word properly");
+  };
+
+  useEffect(() => {
+    requestCameraPermission();
+  }, []);
 
   return (
     <RootContainer>
-      <DWImageInputList
+      {/* <DWImageInputList
         imageUris={imageUris}
         onAddImage={handleAdd}
         onRemoveImage={handleRemove}
-      />
+      /> */}
+
       <DWText>Some text</DWText>
     </RootContainer>
   );
