5e83d8a730143624de9b81092bbe91f1fcf71338
leonel ka <waboleonel@gmail.com>
2024-08-08 12:35:35 +0100

    Basic stack navigation

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
-import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StyleSheet, Text, View } from "react-native";
 import color from "./app/config/color";
-import PostListingScreen from "./app/screens/post/PostListingScreen";
+
+const UserScreen = () => (
+  <View>
+    <Text>User screen</Text>
+  </View>
+);
+
+const UserDetailScreen = () => (
+  <View>
+    <Text>User detail screen</Text>
+  </View>
+);
+
+const Stack = createNativeStackNavigator();
+
+const StackNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen
+      name="user"
+      component={UserScreen}
+      options={{ title: "User | wbt" }}
+    />
+    <Stack.Screen
+      name="user-details"
+      component={UserDetailScreen}
+      options={{ title: "Details | wbt" }}
+    />
+  </Stack.Navigator>
+);
 
 export default function App() {
   return (
-    <>
-      <PostListingScreen />
-    </>
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
   );
 }
 
