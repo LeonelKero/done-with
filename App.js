bc358490db7b94c93d26c0c7bae63a4101484a83
leonel ka <waboleonel@gmail.com>
2024-08-10 08:57:36 +0100

    Basic bottom tab bar navigation

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Button, Pressable, StyleSheet, Text, View } from "react-native";
@@ -15,7 +16,7 @@ const UserScreen = ({ navigation }) => (
 
 const UserDetailScreen = ({ navigation, route }) => (
   <View>
-    <Text>User detail screen with parameter {route.params.id}</Text>
+    <Text>User detail screen with parameter {route.params?.id}</Text>
     <Pressable onPress={() => navigation.navigate("user")}>
       <Text>Go Home</Text>
     </Pressable>
@@ -23,6 +24,7 @@ const UserDetailScreen = ({ navigation, route }) => (
 );
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const StackNavigator = () => (
   <Stack.Navigator
@@ -47,10 +49,26 @@ const StackNavigator = () => (
   </Stack.Navigator>
 );
 
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: "tomato" },
+        headerTintColor: "white",
+        tabBarStyle: {backgroundColor: 'white'}
+      }}
+    >
+      <Tab.Screen name="user" component={UserScreen} options={{tabBarLabel: 'My User'}}/>
+      <Tab.Screen name="user-details" component={UserDetailScreen} />
+    </Tab.Navigator>
+  );
+};
+
 export default function App() {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      {/* <StackNavigator /> */}
+      <TabNavigator />
     </NavigationContainer>
   );
 }
