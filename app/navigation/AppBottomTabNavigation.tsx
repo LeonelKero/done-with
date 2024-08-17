b8c4ab365e1030325ad4bcf7d9166206762fb3e5
leonel ka <waboleonel@gmail.com>
2024-08-17 15:45:04 +0100

    Nested navigation

--- app/navigation/AppBottomTabNavigation.tsx
+++ app/navigation/AppBottomTabNavigation.tsx
@@ -4,13 +4,14 @@ import { StyleSheet } from "react-native";
 import AccountScreen from "../screens/account/AccountScreen";
 import ListingsScreen from "../screens/listings/ListingsScreen";
 import PostListingScreen from "../screens/post/PostListingScreen";
+import FeedNavigation from "./FeedsNavigation";
 
 const BottomTab = createBottomTabNavigator();
 
 const AppBottomTabBar = () => {
   return (
     <BottomTab.Navigator>
-      <BottomTab.Screen name="Feeds" component={ListingsScreen} />
+      <BottomTab.Screen name="Feeds" component={FeedNavigation} />
       <BottomTab.Screen name="Create Post" component={PostListingScreen} />
       <BottomTab.Screen name="Account" component={AccountScreen} />
     </BottomTab.Navigator>
