b05850999d497ff342c934583502aa95c74d11b3
leonel ka <waboleonel@gmail.com>
2024-08-18 20:14:47 +0100

    Define Account navigation

---
+++ app/navigation/AccountNavigation.tsx
@@ -0,0 +1,17 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+import AccountScreen from "../screens/account/AccountScreen";
+import MessagesScreen from "../screens/messages/MessagesScreen";
+
+const Stack = createNativeStackNavigator();
+
+const AccountNavigation = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Account" component={AccountScreen} />
+      <Stack.Screen name="Messages" component={MessagesScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default AccountNavigation;
