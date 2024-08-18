5c07e3a2ebebf2f1a2b3a9124db5609ecfe2e41e
leonel ka <waboleonel@gmail.com>
2024-08-18 21:29:40 +0100

    Define app routes file

---
+++ app/navigation/AppRoutes.ts
@@ -0,0 +1,12 @@
+export default Object.freeze({
+    WELCOME: "Welcome",
+    FEEDS: "Feeds",
+    ALL_FEEDS: "All Feeds",
+    FEEDS_DETAILS: "Feed Details",
+    SIGN_IN: "Sign In",
+    SIGN_UP: "Sign Up",
+    ACCOUNT: "Account",
+    CREATE_POST: "Create Post",
+    MY_ACCOUNT: "My Account",
+    MY_MESSAGES: "My Messages"
+})
