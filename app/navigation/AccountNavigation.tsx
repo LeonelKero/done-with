f83518a9412757f2602eb91bbf0f936849cdea24
leonel ka <waboleonel@gmail.com>
2024-08-18 20:27:07 +0100

    Code refactoring, remove props

--- app/navigation/AccountNavigation.tsx
+++ app/navigation/AccountNavigation.tsx
@@ -8,7 +8,7 @@ const Stack = createNativeStackNavigator();
 const AccountNavigation = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Account" component={AccountScreen} />
+      <Stack.Screen name="My Account" component={AccountScreen} />
       <Stack.Screen name="Messages" component={MessagesScreen} />
     </Stack.Navigator>
   );
