045fe96375559481cc6b2b4a1f8daf1b2ee58132
leonel ka <waboleonel@gmail.com>
2024-08-27 05:26:11 +0100

    Code refactoring, make base url generic

--- app/api/client.ts
+++ app/api/client.ts
@@ -1,5 +1,5 @@
 import { create } from "apisauce";
 
-const apiClient = create({ baseURL: "http://127.0.0.1:8989/api/v1/posts" })
+const apiClient = create({ baseURL: "http://127.0.0.1:8989/api/v1" })
 
 export default apiClient
