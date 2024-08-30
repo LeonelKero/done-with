0589a16164384c8301288c8e4bc875a86d8fa21d
leonel ka <waboleonel@gmail.com>
2024-08-30 22:32:25 +0100

    General API get one element API method

--- app/api/doneWithApi.ts
+++ app/api/doneWithApi.ts
@@ -1,3 +1,5 @@
 import apiClient from "./client";
 
 export const getAll = async <T>(endpoint: string) => apiClient.get<T[]>(endpoint)
+
+export const getOne = async <T>(endpoint: string) => apiClient.get<T>(endpoint)
