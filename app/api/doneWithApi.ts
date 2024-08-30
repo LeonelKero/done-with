cb66b5aa86752af301c5a755527727b0c9b67a97
leonel ka <waboleonel@gmail.com>
2024-08-30 22:35:01 +0100

    General API delete one

--- app/api/doneWithApi.ts
+++ app/api/doneWithApi.ts
@@ -3,3 +3,5 @@ import apiClient from "./client";
 export const getAll = async <T>(endpoint: string) => apiClient.get<T[]>(endpoint)
 
 export const getOne = async <T>(endpoint: string) => apiClient.get<T>(endpoint)
+
+export const deleteOne = async <T>(endpoint: string) => apiClient.delete<T>(endpoint)
