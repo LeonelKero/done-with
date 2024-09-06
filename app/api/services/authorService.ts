172c065618d554a27fd58d3118f371ed6f1be24e
leonel ka <waboleonel@gmail.com>
2024-09-06 22:39:30 +0100

    Implement author service getAll method

---
+++ app/api/services/authorService.ts
@@ -0,0 +1,10 @@
+import { getAll } from "../doneWithApi"
+import Author from "../model/Author"
+
+const authorEndpoint = "/authors"
+
+export const getAllAuthors = async () => {
+    const authors = await getAll<Author>(authorEndpoint)
+    // log data to the console
+    console.log(authors.data.map(author => author.email))
+}
